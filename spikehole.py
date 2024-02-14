dac764ecf6d7af55792fececff527ea7980da5ad
Cyrille Rossant <rossant@users.noreply.github.com>
2024-02-14 15:15:09 +0100

    Rerun pyks2

--- spikehole.py
+++ spikehole.py
@@ -4,6 +4,7 @@
 
 from pathlib import Path
 import shutil
+from urllib.parse import urlparse
 
 import joblib
 import numpy as np
@@ -12,9 +13,11 @@ import matplotlib.pyplot as plt
 
 from one.api import ONE
 from one.remote import aws
+import one.alf.io as alfio
 from ibllib.plots import Density
 from neurodsp.voltage import destripe
 from brainbox.io.one import SpikeSortingLoader
+from pykilosort.ibl import run_spike_sorting_ibl, ibl_pykilosort_params, download_test_data
 
 
 # -------------------------------------------------------------------------------------------------
@@ -24,6 +27,7 @@ from brainbox.io.one import SpikeSortingLoader
 WIN_SIZE = .1
 ROOT_DIR = Path(__file__).resolve().parent
 DATA_DIR = ROOT_DIR / 'data/'
+SCRATCH_DIR = ROOT_DIR / 'scratch'
 PYKS2_DIR = (ROOT_DIR / '..').resolve()
 GLOBAL_ONE = None
 
@@ -48,11 +52,22 @@ def make_snippet(pid, t, win_size=WIN_SIZE):
     return (pid, t0, t1)
 
 
-def get_plot_path(snippet):
-    pid, t0, t1 = snippet
+def get_snippet_path(snippet):
     pid, t0, t1 = snippet
     t = (t0 + t1) / 2.0
-    return DATA_DIR / pid / f'T{t:.3f}'
+    return DATA_DIR / "snippets" / pid / f'T{t:.3f}'
+
+
+def get_cbin_snippet_path(snippet, sr_ap):
+    """Recover the path to the .cbin cached to disk, to run pyks2 on it."""
+    pid, t0, t1 = snippet
+    path = DATA_DIR / 'cache' / 'ap' / urlparse(sr_ap.url_cbin).path[1:]
+    dir = path.parent.resolve()
+    name = path.name
+    l = list(name.split('.'))
+    l[3] = 'stream'
+    name = '.'.join(l)
+    return dir / f'chunk_{t0-1:06d}_to_{t1-1:06d}' / name
 
 
 # -------------------------------------------------------------------------------------------------
@@ -69,7 +84,7 @@ def download_raw_data(snippet):
 
     first, last = (int(t0 * sr_ap.fs), int(t1 * sr_ap.fs))
     raw_ap = sr_ap[first:last, :-sr_ap.nsync].T
-    return raw_ap, sr_ap.fs
+    return raw_ap, sr_ap
 
 
 def download_spike_data(snippet):
@@ -84,8 +99,29 @@ def download_spike_data(snippet):
 # Spike sorting
 # -------------------------------------------------------------------------------------------------
 
-def run_pyks2(snippet):
-    # TODO
+def run_pyks2(snippet, bin_file, force=False):
+    # Get the pyks2 parameters.
+    params = ibl_pykilosort_params(bin_file)
+    params['Th'] = [6, 3]
+
+    # pyks2 output directory.
+    ks_output_dir = get_snippet_path(snippet)
+    ks_output_dir.mkdir(exist_ok=True, parents=True)
+
+    alf_path = ks_output_dir / 'alf'
+    alf_path.mkdir(exist_ok=True, parents=True)
+
+    # Run the spike sorting on the raw data snippet.
+    if force or not (alf_path / 'spikes.samples.npy').exists():
+        print("Running spike sorting...")
+        run_spike_sorting_ibl(
+            bin_file, scratch_dir=SCRATCH_DIR, ks_output_dir=ks_output_dir, alf_path=alf_path,
+            delete=False, log_level='INFO', params=params)
+
+    spikes = alfio.load_object(alf_path, 'spikes')
+    clusters = alfio.load_object(alf_path, 'clusters')
+    channels = alfio.load_object(alf_path, 'channels')
+
     return spikes, clusters, channels
 
 
@@ -121,7 +157,9 @@ def select_times_channels(spikes, clusters, show_interval):
     return si, ci
 
 
-def make_plot(snippet, raw, fs=None, show_interval=None, spikes=None, clusters=None):
+def make_plot(
+        snippet, raw, fs=None, show_interval=None, spikes=None, clusters=None,
+        spikes_rerun=None, clusters_rerun=None):
     assert fs
     pid, t0, t1 = snippet
 
@@ -144,10 +182,11 @@ def make_plot(snippet, raw, fs=None, show_interval=None, spikes=None, clusters=N
 
     # Overlay spikes.
     if spikes is not None and clusters is not None:
-        s = spikes.times
-        c = clusters.channels[spikes.clusters]
         si, ci = select_times_channels(spikes, clusters, show_interval)
-        ax.scatter(si, ci, s=20, color=[1, 0, 0], alpha=.5)
+        ax.scatter(si, ci, s=50, color=[1, 0, 0], alpha=.5)
+    if spikes_rerun is not None and clusters_rerun is not None:
+        si, ci = select_times_channels(spikes_rerun, clusters_rerun, show_interval)
+        ax.scatter(si, ci, s=50, color=[0, 1, 0], alpha=.5)
 
     # Figure title.
     ax.set_title(f"{pid}: {t0s}-{t1s}")
@@ -176,16 +215,22 @@ if __name__ == '__main__':
     snippet = (pid, t0, t1)
     show_interval = (T, T + dt)
 
-    # snippet = make_snippet(pid, T, win_size=10)
-    # path = get_plot_path(snippet)
-
     # Raw data.
-    raw_ap, fs = download_raw_data(snippet)
+    raw_ap, sr_ap = download_raw_data(snippet)
+    fs = sr_ap.fs
 
     # Spikes.
-    spikes, clusters, channels = download_spike_data(snippet)
-    s = spikes.times
-    c = clusters.channels[spikes.clusters]
 
+    # Load existing spikes.
+    spikes_orig, clusters_orig, channels_orig = download_spike_data(snippet)
+
+    # Also, run spike sorting again.
+    spikes_rerun, clusters_rerun, channels_rerun = run_pyks2(
+        snippet, get_cbin_snippet_path(snippet, sr_ap))
+    spikes_rerun.times += t0 - 1
+
+    # Show the spikes on top of the raw data.
     make_plot(
-        snippet, raw_ap, fs=fs, show_interval=show_interval, spikes=spikes, clusters=clusters)
+        snippet, raw_ap, fs=fs, show_interval=show_interval,
+        spikes=spikes_orig, clusters=clusters_orig,
+        spikes_rerun=spikes_rerun, clusters_rerun=clusters_rerun)
