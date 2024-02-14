560cb93cf94552d57071b009429b29c1706a6a17
Cyrille Rossant <rossant@users.noreply.github.com>
2024-02-14 11:44:25 +0100

    Display spikes on top of raw data

--- spikehole.py
+++ spikehole.py
@@ -72,17 +72,32 @@ def download_raw_data(snippet):
     return raw_ap, sr_ap.fs
 
 
+def download_spike_data(snippet):
+    pid, t0, t1 = snippet
+    one = get_one()
+    ssl = SpikeSortingLoader(pid=pid, one=one)
+    spikes, clusters, channels = ssl.load_spike_sorting()
+    return spikes, clusters, channels
+
+
 # -------------------------------------------------------------------------------------------------
 # Plotting
 # -------------------------------------------------------------------------------------------------
 
-def make_plot(snippet, raw, fs=None, spike_times=None):
+def make_plot(snippet, raw, fs=None, spikes=None, clusters=None):
     assert fs
     pid, t0, t1 = snippet
     fig, ax = plt.subplots(figsize=(12, 6))
-    # ax.imshow(raw)
     vmin = vmax = None
     d1 = Density(raw, fs=fs, taxis=1, ax=ax, vmin=vmin, vmax=vmax, cmap='Greys')
+    if spikes is not None and clusters is not None:
+        s = spikes.times
+        c = clusters.channels[spikes.clusters]
+        # Subselect spikes.
+        idx = (s >= t0) & (s < t1)
+        si = 1000 * (s[idx] - t0)
+        ci = c[idx]
+        ax.scatter(si, ci, s=10, c=[1, 1, 0])
     ax.set_title(f"{pid}: {t0}-{t1}")
     plt.show()
 
@@ -90,14 +105,27 @@ def make_plot(snippet, raw, fs=None, spike_times=None):
 # -------------------------------------------------------------------------------------------------
 # Entry-point
 # -------------------------------------------------------------------------------------------------
+
 if __name__ == '__main__':
+
     # PIDs.
     pid = '84bb830f-b9ff-4e6b-9296-f458fb41d160'
     # pid = '5a34d971-1cb3-4f0e-8dfe-e51e2313a668'
 
-    snippet = make_snippet(pid, 1500)
-    path = get_plot_path(snippet)
+    T = 1500
+
+    snippet = make_snippet(pid, T)
+    # path = get_plot_path(snippet)
+
+    # Raw data.
     raw_ap, fs = download_raw_data(snippet)
     destriped = destripe(raw_ap, fs=fs)
 
-    make_plot(snippet, destriped, fs=fs)
+    # Spikes.
+    spikes, clusters, channels = download_spike_data(snippet)
+    s = spikes.times
+    c = clusters.channels[spikes.clusters]
+    # print(s.shape)
+    # print(c.shape)
+
+    make_plot(snippet, destriped, fs=fs, spikes=spikes, clusters=clusters)
